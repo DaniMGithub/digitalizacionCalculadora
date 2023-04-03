e4e8fe77f9dc9f4313f649ed65d78ae47a04a8c7
Dani Milano <danimilano6@gmail.com>
2023-04-03 00:18:54 -0300

    más calculadoras

--- js/sript.js
+++ js/sript.js
@@ -5,8 +5,6 @@ if (aceptaCookies) {
   var nombre = prompt("Por favor ingrese su nombre:");
   var apellido = prompt("Por favor ingrese su apellido:");
   var nombreProyecto = prompt("Por favor ingrese el nombre del proyecto de digitalización:");
-  // Si el usuario acepta las cookies y proporciona la información solicitada, se puede realizar alguna acción aquí
-  // Por ejemplo, guardar la información en una cookie o utilizarla para personalizar el sitio web
 } else {
   // Si el usuario no acepta las cookies, se muestra un mensaje de alerta
   alert("Sentimos mucho que no acepte nuestras cookies.");
